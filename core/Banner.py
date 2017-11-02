914c480bd9b9ac005363c1c2df3d41cb53f27700
BL4CKvGHOST <youssefheshamhassan@gmail.com>
2017-11-02 16:22:38 +0200

    added Banner to display the ASCII banner in the terminal

---
+++ core/Banner.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import Obevilion
+
+class Printer:
+    """Just Printing Banners in this class."""
+    def main_banner(self):
+         print('''
+
+        ████▄ ███   ▄███▄      ▄   ▄█ █    ▄█ ████▄    ▄
+        █   █ █  █  █▀   ▀      █  ██ █    ██ █   █     █
+        █   █ █ ▀ ▄ ██▄▄   █     █ ██ █    ██ █   █ ██   █
+        ▀████ █  ▄▀ █▄   ▄▀ █    █ ▐█ ███▄ ▐█ ▀████ █ █  █
+              ███   ▀███▀    █  █   ▐     ▀ ▐       █  █ █
+                              █▐                    █   ██
+                              ▐
+                                   NO ONE WILL GIVE YOU FREEDOM
+                                   YOU TAKE IT!
+        ''')
+         print("OBEVILION")
+         print("Updates: now officialy obevilion is the strongest Cracking Script")
+         print("Coded By BL4CKvGHOST")
+         print("GitHub: https://github.com/BL4CKvGHOST")
+         print("Twitter: https://twitter.com/BL4CKvGHOST")
+         print("Usage: Python3 %s [display_mode] [archive path]" % Obevilion.get_name())
