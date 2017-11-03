b61cee0532645f0e89a4a58f562339b54f3824a2
BL4CKvGHOST <youssefheshamhassan@gmail.com>
2017-11-03 13:03:51 +0200

    calling the script directly

--- Obevilion.py
+++ Obevilion.py
@@ -14,19 +14,18 @@ from core import Banner, gui, Obevilion
 printer = Banner.Printer()
 action = sys.argv[1]
 
-
-def runCLI(arg):
-    if arg is not None:
-        Obevilion.script(path=arg, limit=3)
-    else:
-        printer.main_banner()
+# def runCLI(arg):
+#     if arg is not None:
+#         Obevilion.script(path=arg, limit=3)
+#     else:
+#         printer.main_banner()
 
 def main():
     assert action in ['--gui', '--cli'], "Action is not one of [ --gui or --cli]"
     if action == '--gui':
         gui.main()
     elif action == '--cli':
-        runCLI(sys.argv[2])
+        Obevilion.script(path=sys.argv[2], limit=3)
 
 if __name__ == '__main__':
     main()
