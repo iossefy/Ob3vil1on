a23de43713045f5161eb9d177654601b77a15167
BL4CKvGHOST <youssefheshamhassan@gmail.com>
2017-11-09 08:52:09 +0200

    new option '--easy_mode'
    added --license to view the license of Obevilion
    
    added --about_me to view informations about me
    
    added a function to manage the easy mode (loop mode)
    
    clearing the screen when starting the easy mode (loop mode)

--- Obevilion.py
+++ Obevilion.py
@@ -7,6 +7,7 @@
 # License <http://www.gnu.org/licenses/gpl-3.0.html>
 
 import sys
+import subprocess
 from core import Banner, gui, Obevilion, Check, Control
 
 check_req  = Check.Check_req()
@@ -14,17 +15,33 @@ printer    = Banner.Printer()
 looprocess = Control.LoopControl()
 
 action = ''
-commands = ['--gui', '--cli', '--help', '--about']
+commands = ['--gui', '--cli', '--help', '--about', '--easy_mode', '--about_me', '--license']
 
 try:
     action = sys.argv[1]
 except Exception as e:
     printer.main_banner()
-    looprocess.loop()
+    # looprocess.loop()
+
+def loop_action(action): # Using the easy loop system
+    if action == '--easy_mode':
+        printer.main_banner
+        looprocess.loop()
+    else:
+        pass
 
 def run():
     try:
         assert action in commands, "Action is not one of %s" % ', '.join(map(str, commands))
+        try:
+            if action == '--easy_mode':
+                subprocess.call('clear', shell=True)
+                printer.main_banner()
+                loop_action(action)
+            else:
+                pass
+        except Exception as e:
+            pass
         if action == '--gui':
             gui.main()
         elif action == '--cli':
@@ -33,6 +50,10 @@ def run():
             printer.help_banner()
         elif action == '--about':
             printer.about()
+        elif action == '--about_me':
+            printer.about_me()
+        elif action == '--license':
+            printer.License()
     except Exception as e:
         try:
             if sys.argv[1] is not commands:
