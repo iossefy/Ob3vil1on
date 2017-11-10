c84d1042205a02b928d31207acfab4fdca9e2f6f
BL4CKvGHOST <youssefheshamhassan@gmail.com>
2017-11-10 18:15:10 +0200

    setting up the main skeleton

--- core/UI/jobs.py
+++ core/UI/jobs.py
@@ -5,3 +5,37 @@
 # Copyright (C) 2017 Youssef Hesham
 #
 # License <http://www.gnu.org/licenses/gpl-3.0.html>
+
+class DO(object):
+    """Just call me and i will do
+       what you want me to 'DO'."""
+    def __init__(self, arg):
+        super(DO, self).__init__()
+        self.arg = arg
+
+    def about_qt(self):
+        pass
+
+    def about_me(self):
+        pass
+
+    def about_script(self):
+        pass
+
+    def quit(self):
+        pass
+
+    def clear_output(self):
+        pass
+
+    def save_output(self):
+        pass
+
+    def start_cracking(self):
+        pass
+
+    def choose_archive(self):
+        pass
+
+    def choose_dict(self):
+        pass
