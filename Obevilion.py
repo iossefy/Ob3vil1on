6c1cd013872dfdbd2143adb58e615fab80dfa6b6
BL4CKvGHOST <youssefheshamhassan@gmail.com>
2017-11-10 10:19:28 +0200

    added get_name function

--- Obevilion.py
+++ Obevilion.py
@@ -7,6 +7,7 @@
 # License <http://www.gnu.org/licenses/gpl-3.0.html>
 
 import sys
+import os
 import subprocess
 from core import Banner, Obevilion, Check, Control
 from core.UI import gui
@@ -16,6 +17,30 @@ printer    = Banner.Printer()
 looprocess = Control.LoopControl()
 action     = ''
 commands   = ['--gui', '--cli', '--help', '--about', '--easy_mode', '--about_me', '--license']
+
+
+def get_name(arg=''):
+    '''
+    Managing the file name
+    '''
+    name = os.path.basename(__file__)
+    if arg=='noPy':
+        return name.replace('.py', '')
+    elif arg=='all':
+        if 'pyc' not in name:
+            return name
+        elif 'pyc' in name:
+            return name.replace('.pyc', '')
+        elif 'py' in name:
+            return name.replace('.py', '')
+        else:
+            return None
+    elif arg=='':
+        if "pyc" not in name:
+            return name
+        else:
+            return name.replace('pyc','py')
+        return None
 
 try:
     action = sys.argv[1]
