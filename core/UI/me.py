b491ba7001587f08dad43e06b68d479747cde811
BL4CKvGHOST <youssefheshamhassan@gmail.com>
2017-11-13 13:14:34 +0200

    about_me window

--- core/UI/me.py
+++ core/UI/me.py
@@ -6,4 +6,31 @@
 #
 # License <http://www.gnu.org/licenses/gpl-3.0.html>
 
-import jobs
+from tkinter import *
+import os
+import sys
+
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.init_window()
+
+    def init_window(self):
+        self.master.title("About The Programmer")
+        self.showTxT()
+
+    def showTxT(self):
+        self.TxT  = Label(self.master, text='IAM A PROGRAMMER!', pady=60, padx=60)
+        self.decr = Label(self.master, text="Go Back Fool!", pady=70, padx=70)
+        self.TxT.pack()
+        self.decr.pack()
+
+def main():
+    root = Tk()
+    root.geometry('500x300')
+    app = Window(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
