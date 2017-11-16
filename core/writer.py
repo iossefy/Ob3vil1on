cfdde7dc6e04d316a92006d7f25e5fd69e5d0857
BL4CKvGHOST <youssefheshamhassan@gmail.com>
2017-11-16 14:32:56 +0200

    Writer.py to write to csv files

---
+++ core/writer.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# @author bl4ckvghost
+#
+# Copyright (C) 2017 Youssef Hesham
+#
+# License <http://www.gnu.org/licenses/gpl-3.0.html>
+
+class Booker(object):
+    """docstring for Booker.
+
+    ======================================
+    |CREATING CSV FILES|READING CSV FILES |
+    | :-------------   | :-------------   |
+    |WRITING CSV FILES |DELETING CSV FILES|
+    =======================================
+    
+    """
+    def __init__(self, arg):
+        super(Booker, self).__init__()
+        self.arg = arg
