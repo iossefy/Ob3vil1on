82608bc238e7392e8c880959ff715e0ad5da2ea2
BL4CKvGHOST <youssefheshamhassan@gmail.com>
2017-11-16 15:33:00 +0200

    added new methods
    added read method
    
    added write method

--- core/writer.py
+++ core/writer.py
@@ -6,6 +6,8 @@
 #
 # License <http://www.gnu.org/licenses/gpl-3.0.html>
 
+import csv
+
 class Booker(object):
     """docstring for Booker.
 
@@ -14,8 +16,16 @@ class Booker(object):
     | :-------------   | :-------------   |
     |WRITING CSV FILES |DELETING CSV FILES|
     =======================================
-    
+
     """
-    def __init__(self, arg):
-        super(Booker, self).__init__()
-        self.arg = arg
+
+    def write(self, fileName, password):
+        with open("output.csv", 'a') as csv_file:
+            write = csv.writer(csv_file)
+            write.withrow([fileName, password])
+
+    def read(self):
+        with open("output.csv", 'r') as csv_file:
+            csv_reader = csv.read(csv_file)
+            for line in csv_file:
+                print("{filename}:{password}".format(line[0], line[1]))
