08da56c83e88f4d464d6dc31401400dd7a7a5a0c
BL4CKvGHOST <youssefheshamhassan@gmail.com>
2017-11-17 21:06:35 +0200

    replaced with one line

--- core/writer.py
+++ core/writer.py
@@ -46,5 +46,4 @@ class Booker(object):
         with open(output, 'rb') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=',')
             for line in csv_reader:
-                o = "{filename}:\t{password}".format(filename=line[0], password=line[1])
-        print(o)
+                print("File Path: {filename}:\tPassword: {password}".format(filename=line[0], password=line[1]))
