1819fd050fd28edce980544b5cf3c55930c843fd
BL4CKvGHOST <youssefheshamhassan@gmail.com>
2017-11-17 14:25:02 +0200

    bug fixed
    -- for line in csv_file [CHANGED WITH] for line in csv_reader

--- core/writer.py
+++ core/writer.py
@@ -8,6 +8,7 @@
 
 import csv
 
+
 class Booker(object):
     """docstring for Booker.
 
@@ -20,12 +21,17 @@ class Booker(object):
     """
 
     def write(self, fileName, password):
+        """
+        fileName: Getting the file name to write it into file_name column.
+        password: Getting the Password to write it into password column.
+        """
         with open("output.csv", 'a') as csv_file:
             write = csv.writer(csv_file)
             write.withrow([fileName, password])
 
     def read(self):
+        """Reading the Information of the cracked files."""
         with open("output.csv", 'r') as csv_file:
             csv_reader = csv.read(csv_file)
-            for line in csv_file:
+            for line in csv_reader:
                 print("{filename}:{password}".format(line[0], line[1]))
