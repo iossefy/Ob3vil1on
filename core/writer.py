01fafdaf7c592012ed85cafa7a694c2ce3f96cf5
BL4CKvGHOST <youssefheshamhassan@gmail.com>
2017-11-17 15:45:11 +0200

    docstring updated

--- core/writer.py
+++ core/writer.py
@@ -18,20 +18,30 @@ class Booker(object):
     |WRITING CSV FILES |DELETING CSV FILES|
     =======================================
 
+    write: write to csv file.
+    write argument:filename, password
+    filename: pass the file name to write to file_name column in csv file
+    password: pass the password to write to password column in csv file
+
+    read: read the contents of the csv file.
+    read arguments: output
+    output: not a required function, you can pass the csv file in there
+
     """
 
-    def write(self, fileName, password):
+    def write(self, fileName, password, output="output.csv"):
         """
         fileName: Getting the file name to write it into file_name column.
         password: Getting the Password to write it into password column.
         """
-        with open("output.csv", 'a') as csv_file:
+        with open(output, 'a') as csv_file:
             write = csv.writer(csv_file)
             write.withrow([fileName, password])
 
-    def read(self):
-        """Reading the Information of the cracked files."""
-        with open("output.csv", 'r') as csv_file:
+    def read(self, output="output.csv"):
+        """Reading the Information of the cracked files.
+           output: not a required function, you can pass the csv file in there."""
+        with open(output, 'r') as csv_file:
             csv_reader = csv.read(csv_file)
             for line in csv_reader:
                 print("{filename}:{password}".format(line[0], line[1]))
