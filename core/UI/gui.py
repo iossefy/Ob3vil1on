9dff7b5288ce17373445204e29ae75cdb9008f8a
BL4CKvGHOST <youssefheshamhassan@gmail.com>
2017-12-12 09:30:44 +0200

    importerror: pyqt4 not found

--- core/UI/gui.py
+++ core/UI/gui.py
@@ -7,8 +7,12 @@
 # License <http://www.gnu.org/licenses/gpl-3.0.html>
 
 import sys
-from PyQt4 import QtCore, QtGui
-from PyQt4.QtGui import *
+try:
+
+    from PyQt4 import QtCore, QtGui
+    from PyQt4.QtGui import *
+except Exception as e:
+    print("Python PyQt4 Not Found!")
 from jobs import DO
 
 try:
