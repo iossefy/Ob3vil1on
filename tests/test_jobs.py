c663d9dc65cc2f6f7114f5d2f759d2184f7a370d
BL4CKvGHOST <youssefheshamhassan@gmail.com>
2017-12-12 16:37:25 +0200

    tests folder

---
+++ tests/test_jobs.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+
+# @author bl4ckvghost
+#
+# Copyright (C) 2017 Youssef Hesham
+#
+# License <http://www.gnu.org/licenses/gpl-3.0.html>
+
+from PyQt4 import QtCore, QtGui
+import qt
+import os
+import me
+import ascript
+import attack
+import sys
+import subprocess
+import tkinter as tk
+import tkinter.messagebox
+
+
+class DO(object):
+    """Just call me and i will do
+       what you want me to 'DO'."""
+
+    def about_qt(self, output):
+        '''
+        Getting the path of the current directory then
+        run qt.py in 'core/UI/qt.py'
+        '''
+        try:
+            subprocess.call(
+                'python {}/core/UI/qt.py'.format(os.getcwd()), shell=True)  # Call qt.py script
+            output.append("Open About Qt")  # Logging to the console
+        except Exception as e:
+            output.append(e)
+
+    def about_me(self, output):
+        try:
+            me.main()  # Call this function
+            output.append("Open About Window.")
+        except Exception as e:
+            output.append(e)
+
+    def about_script(self, output):
+        """
+        Calling about the script.
+        this script runs ascript.py
+        """
+        try:
+            ascript.main()  # Call this function
+            output.append("Open About Script")
+        except Exception as e:
+            output.append(e)
+
+    def quit(self, output=None):
+        '''
+        Displaying a dialog window for the user
+        to confirm if the user want to quit.
+        '''
+        try:
+            root = tk.Tk()  # root window
+            root.withdraw()
+            # Ask a question
+            answer = tk.messagebox.askquestion(
+                "Quit", "Are you sure\nYou want to exit?")
+            if answer == 'yes':  # if the user said yes
+                sys.exit(0)
+            elif answer == 'no':  # if the user said no
+                root.destroy()  # Terminating the window
+                if output != None:
+                    # Logging to the console
+                    output.append("Quit action canceled by user")
+                else:
+                    pass
+        except Exception as e:
+            output.append(e)
+
+    def clear_output(self, output):
+        """Clear the output"""
+        try:
+            output.clear()  # Clear output console
+        except Exception as e:
+            output.append(e)
+
+    def start_cracking(self, radio, textField, path=None, passlist=None):
+        """
+        The main function to manage cracking methods (BruteForce / Dictionary) Attacks.
+
+        Parameters:
+        radio: this will be the bruteforce radio by default.
+        textField: the output textfield that the password display on.
+        path: the path of archive file.
+        passlist: (optional) for dictionary attack.
+        """
+        try:
+            if radio.isChecked() == True:
+                self.bruteforce(path, textField)
+            else:
+                if passlist == '' or passlist is None:
+                    textField.append(
+                        "Please enter a password list to start attack")
+                else:
+                    self.dictionary(path, textField, passlist)
+        except Exception as e:
+            textField.append(e)
+
+    def bruteforce(self, path, textField):
+        """
+        Bruteforce attack code goes here.
+        path: path of the target Archive File.
+        textField: the output textfield.
+        """
+        try:
+            attack.rc(path, textField)
+        except Exception as e:
+            textField.append(e)
+
+    def dictionary(self, path, textField, passlist):
+        """
+        Dictionary attack code goes here.
+        path: path of the target Archive File.
+        textField: the output textfield.
+        passlist: password list to iterate on.
+        """
+        try:
+            textField.append("Not Available Right Now!")
+        except Exception as e:
+            textField.append(e)
+
+    def change_theme(self, theme, output):
+        """
+        Changing The Theme.
+        The user can choose from the following themes,
+
+        ["motif", "windows", "cde",
+        "Plastique", "Cleanlooks", "gtk+"]
+        """
+        try:
+            QtGui.QApplication.setStyle(
+                QtGui.QStyleFactory.create(theme))  # Changing the theme
+            output.append("Theme Changed")  # Logging to the console
+        except Exception as e:
+            output.append(e)
+
+    def radioState(self, radio, field, btn, output):
+        '''
+        Checking if the BruteForce Attack radio button
+        is Checked, disable the dictionary attack text
+        field and dictionary attack 'choose' button,
+        if not enable them.
+
+        radio: is for the choosen radio button.
+        field: is for the choosen text field.
+        btn: is for the choosen 'choose' button.
+        '''
+        try:
+            if radio.isChecked() == True:  # if the radio button is checked
+                field.setDisabled(True)  # disable text field
+                btn.setDisabled(True)  # disable button
+                output.append("Bruteforce attack enabled")
+            elif radio.isChecked() == False:  # if the radio button is not checked
+                field.setDisabled(False)  # enable text field
+                btn.setDisabled(False)  # enable button
+                # Logging to the console
+                output.append("Dictionary attack enabled")
+        except Exception as e:
+            output.append(e)
+
+    def open_file(self, field, output):
+        """
+        Open file dialog to let the user choose the
+        file easily without entering it manualy.
+        """
+        try:
+            field.setText(QtGui.QFileDialog.getOpenFileName())
+            output.append("Open File Chooser")
+        except Exception as e:
+            output.append(e)
